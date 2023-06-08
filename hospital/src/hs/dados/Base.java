19de3e8e96a80e14b4f8052f10340ed67239998f
fc <poo>
2023-06-08 16:40:29 -0300

    atualização

--- hospital/src/hs/dados/Base.java
+++ hospital/src/hs/dados/Base.java
@@ -1,5 +1,18 @@
 package hs.dados;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hs.modelos.Pacientes;
+
 public class Base {
+	
+	
+	public List<Pacientes> professores(){
+		List<Pacientes> lista = new ArrayList<Pacientes>();
+		for (int i = 0; i < 10; i++) 
+			lista.add(new Pacientes( (i+1), "Paciente" + (((i * 6) + 6) < 10 ? "0" : "") + ((i * 6) + 6)));
+		return lista;
+	}
 
 }
